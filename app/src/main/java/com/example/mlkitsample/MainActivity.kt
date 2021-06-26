e7a70bc5a5a093d3aab79b56745346d770ea2d82
한경동 <hkd0694@gmail.com>
2021-06-27 01:50:36 +0900

    사람일 경우 캐치 성공

--- app/src/main/java/com/example/mlkitsample/MainActivity.kt
+++ app/src/main/java/com/example/mlkitsample/MainActivity.kt
@@ -169,48 +169,87 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     poseDetector.process(image)
                         .addOnSuccessListener { pose ->
                             var allPoseLandmarks = pose.allPoseLandmarks
-                            val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
-                            val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
-                            val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
-                            val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
-                            val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
-                            val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
-                            val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
-                            val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
-                            val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
-                            val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
-                            val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
-                            val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
-                            val leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY)
-                            val rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY)
-                            val leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX)
-                            val rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX)
-                            val leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB)
-                            val rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB)
-                            val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
-                            val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
-                            val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
-                            val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
-                            val nose = pose.getPoseLandmark(PoseLandmark.NOSE)
-                            val leftEyeInner = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_INNER)
-                            val leftEye = pose.getPoseLandmark(PoseLandmark.LEFT_EYE)
-                            val leftEyeOuter = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_OUTER)
-                            val rightEyeInner = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_INNER)
-                            val rightEye = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE)
-                            val rightEyeOuter = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_OUTER)
-                            val leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)
-                            val rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)
-                            val leftMouth = pose.getPoseLandmark(PoseLandmark.LEFT_MOUTH)
-                            val rightMouth = pose.getPoseLandmark(PoseLandmark.RIGHT_MOUTH)
-                            if( leftShoulder != null ) {
-                                var position = leftShoulder.position
-                                if( null != position ) {
-
-                                }
+                            if( allPoseLandmarks.size == 0 ) {
+                                Log.d("asd", "사람 없음 ")
+                                imageProxy.close()
+                            } else {
+                                tv_acc.text = "정확도 : ${allPoseLandmarks[PoseLandmark.NOSE].inFrameLikelihood}"
+                                Log.d("asd", "캐치 성공! ${allPoseLandmarks[PoseLandmark.NOSE].landmarkType} : ${allPoseLandmarks[PoseLandmark.NOSE].inFrameLikelihood} ")
+                                imageProxy.close()
                             }
+                            // // 왼쪽 어깨
+                            // val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+                            // // 오른쪽 어깨
+                            // val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
+                            // // 왼쪽 팔꿈치
+                            // val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
+                            // // 오른쪽 팔꿈치
+                            // val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+                            // // 왼쪽 손목
+                            // val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
+                            // // 오른쪽 손목
+                            // val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
+                            // // 왼쪽 엉덩이
+                            // val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
+                            // // 오른쪽 엉덩이
+                            // val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+                            // // 왼쪽 무릎
+                            // val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
+                            // // 오른쪽 무릎
+                            // val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
+                            // // 왼쪽 발목
+                            // val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
+                            // // 오른쪽 발목
+                            // val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
+                            // // 왼쪽 새끼 손가락
+                            // val leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY)
+                            // // 오른쪽 새끼 손가락
+                            // val rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY)
+                            // // 왼쪽 검지 손가락
+                            // val leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX)
+                            // // 오른쪽 검지 손가락
+                            // val rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX)
+                            // // 왼쪽 엄지 손가락
+                            // val leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB)
+                            // // 오른쪽 엄지 손가락
+                            // val rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB)
+                            // // 왼쪽 뒤꿈치
+                            // val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
+                            // // 오른쪽 뒤꿈치
+                            // val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
+                            // // 왼쪽 검지 발가락
+                            // val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
+                            // // 오른쪽 검지 발가락
+                            // val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
+                            // // 코
+                            // val nose = pose.getPoseLandmark(PoseLandmark.NOSE)
+                            // // 왼쪽 눈 안
+                            // val leftEyeInner = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_INNER)
+                            // // 왼쪽 눈
+                            // val leftEye = pose.getPoseLandmark(PoseLandmark.LEFT_EYE)
+                            // // 왼쪽 눈 바깥
+                            // val leftEyeOuter = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_OUTER)
+                            // // 오른쪽 눈 안
+                            // val rightEyeInner = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_INNER)
+                            // // 오른쪽 눈
+                            // val rightEye = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE)
+                            // // 오른쪽 눈 바깥
+                            // val rightEyeOuter = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_OUTER)
+                            // // 왼쪽 귀
+                            // val leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)
+                            // // 오른쪽 귀
+                            // val rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)
+                            // // 왼쪽 입
+                            // val leftMouth = pose.getPoseLandmark(PoseLandmark.LEFT_MOUTH)
+                            // // 오른쪽 입
+                            // val rightMouth = pose.getPoseLandmark(PoseLandmark.RIGHT_MOUTH)
+                            // tv_acc.text = "정확도 : ${leftShoulder.inFrameLikelihood}"
+                            // if( leftShoulder != null ) {
+                            //     var position = leftShoulder.position
+                            //     if( null != position ) {
+                            //     }
+                            // }
                             // 이걸 안쓰면 화면 멈춤! ( 이미지를 닫아 줘야 다음 프레임을 가져오기때문에 꼭 해줘야함! )
-                            Log.d("asd", "성공! ")
-                            imageProxy.close()
                         }
                         .addOnFailureListener { e ->
                             // 이걸 안쓰면 화면 멈춤! ( 이미지를 닫아 줘야 다음 프레임을 가져오기때문에 꼭 해줘야함! )
